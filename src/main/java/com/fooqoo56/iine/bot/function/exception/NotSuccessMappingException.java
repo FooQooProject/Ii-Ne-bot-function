1ec3046ba7079861aa728b2cbca78210b94b2432
FooQoo <horseshoes.buzz.17@gmail.com>
2021-03-07 00:35:40 +0900

    コメント追加

--- src/main/java/com/fooqoo56/iine/bot/function/exception/NotSuccessMappingException.java
+++ src/main/java/com/fooqoo56/iine/bot/function/exception/NotSuccessMappingException.java
@@ -1,10 +1,18 @@
 package com.fooqoo56.iine.bot.function.exception;
 
+/**
+ * pub/subメッセージからオブジェクトへのマッピングした場合の例外
+ */
 public class NotSuccessMappingException extends IiNeBotException {
 
     private static final long serialVersionUID = -164643728314322740L;
 
+    /**
+     * コンストラクタ
+     *
+     * @param throwable 例外
+     */
     public NotSuccessMappingException(final Throwable throwable) {
-        super("pubsubメッセージからオブジェクトへのマッピングに失敗しました:", throwable);
+        super("pub/subメッセージからオブジェクトへのマッピングに失敗しました:", throwable);
     }
 }
